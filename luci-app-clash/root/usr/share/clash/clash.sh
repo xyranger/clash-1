7fa15f5a0609acf8db3b8e17b854085e817ee872
vernesong <42875168+vernesong@users.noreply.github.com>
2019-05-04 12:25:53 +0800

    Revert "add some feature"
    This reverts commit 755a5ccd700578c9ef2c810f195c5927d4276a57.

--- luci-app-clash/root/usr/share/clash/clash.sh
+++ luci-app-clash/root/usr/share/clash/clash.sh
@@ -2,7 +2,5 @@
 subscribe_url=$(uci get clash.config.subscribe_url 2>/dev/null)
 rm -rf /etc/clash/config.bak 2> /dev/null
 wget-ssl --user-agent="User-Agent: Mozilla" $subscribe_url -O 2>&1 >1 /etc/clash/config.yml
-[ "$?" -ne "0" ] && {
-   wget-ssl -O 2>&1 >1 /etc/clash/config.yml "$subscribe_url"
-}
-/etc/init.d/clash restart
+/etc/init.d/clash restart >/dev/null 2>&1
+
