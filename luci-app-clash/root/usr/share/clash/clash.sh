78f3baa1226d1579eb495a699e1aabe5b9133a27
vernesong <42875168+vernesong@users.noreply.github.com>
2019-05-04 11:49:43 +0800

    add some feature

--- luci-app-clash/root/usr/share/clash/clash.sh
+++ luci-app-clash/root/usr/share/clash/clash.sh
@@ -2,5 +2,7 @@
 subscribe_url=$(uci get clash.config.subscribe_url 2>/dev/null)
 rm -rf /etc/clash/config.bak 2> /dev/null
 wget-ssl --user-agent="User-Agent: Mozilla" $subscribe_url -O 2>&1 >1 /etc/clash/config.yml
-/etc/init.d/clash restart >/dev/null 2>&1
-
+[ "$?" -ne "0" ] && {
+   wget-ssl -O 2>&1 >1 /etc/clash/config.yml "$subscribe_url"
+}
+/etc/init.d/clash restart
